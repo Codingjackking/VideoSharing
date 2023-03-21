f97ff2bd70d1650da8b244c68fc513aca33c02ea
Naing <nainghtet123@gmail.com>
2023-03-20 20:36:32 -0700

    Revert "update 1: Finished Registration ability to register accounts and working on hamburger menu"
    This reverts commit b0be5b1889b92b32ed71d81b5b01689e4c7e8f1d.
    git commit -m "update 1: Finished Registration ability to register
    accounts and working on hamburger menu"

--- application/public/js/menu.js
+++
@@ -1,6 +0,0 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const menu = document.querySelector('.menu');
-
-hamburgerMenu.addEventListener('click', function() {
-  menu.classList.toggle('show');
-});
